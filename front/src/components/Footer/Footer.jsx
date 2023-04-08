092e488020471b93bde5233da9f1b26459ff8fa6
nahucham18 <nahu.chamo19@gmail.com>
2023-04-07 23:58:38 -0300

    Agregado de redes  en footer

--- front/src/components/Footer/Footer.jsx
+++ front/src/components/Footer/Footer.jsx
@@ -1,9 +1,15 @@
 import style from './Footer.module.css'
+import imgWpp from '../../assets/imagenes/whatsapp_icon.png'
+import imgLink from '../../assets/imagenes/linkedin_icon.png'
+import imgGit from '../../assets/imagenes/github_icon.png'
 
 const Footer = () =>{
     return (
         <footer className={style.footer}>
             <p className={style.p}>Creado por Nahuel Chamorro</p>
+            <img src={imgWpp} alt='whatsapp' />
+            <img src={imgLink} alt='Linkedin' />
+            <img src={imgGit} alt='GitHub' />
         </footer>
     )
 }
